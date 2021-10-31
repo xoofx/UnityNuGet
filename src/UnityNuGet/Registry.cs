3ddd30c180e91e7ab0a625223532d660a69d4417
bdovaz <iam.n3uro@gmail.com>
2021-10-31 19:23:49 +0100

    Implement ISerializable correctly
    https://rules.sonarsource.com/csharp/RSPEC-3925

--- src/UnityNuGet/Registry.cs
+++ src/UnityNuGet/Registry.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization;
 using Newtonsoft.Json;
 
 namespace UnityNuGet
@@ -8,11 +9,16 @@ namespace UnityNuGet
     /// <summary>
     /// Loads the `registry.json` file at startup
     /// </summary>
+    [Serializable]
     public sealed class Registry : Dictionary<string, RegistryEntry>
     {
         private const string RegistryFileName = "registry.json";
         private static readonly object LockRead = new();
         private static Registry _registry = null;
+
+        private Registry(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
 
         public static Registry Parse(string json)
         {
