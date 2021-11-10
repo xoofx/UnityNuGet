8acbe1f8dac30c4f7fb98abe490361ef6ae0c52f
bdovaz <iam.n3uro@gmail.com>
2021-11-10 18:46:08 +0100

    Test registry sort

---
+++ src/UnityNuGet.Tests/RegistryTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace UnityNuGet.Tests
+{
+    public class RegistryTests
+    {
+        [Test]
+        public void TestSort()
+        {
+            var registry = Registry.GetInstance();
+            var originalPackageNames = registry.Select(r => r.Key).ToArray();
+            var sortedPackageNames = originalPackageNames.OrderBy(p => p).ToArray();
+
+            Assert.AreEqual(sortedPackageNames, originalPackageNames);
+        }
+    }
+}
