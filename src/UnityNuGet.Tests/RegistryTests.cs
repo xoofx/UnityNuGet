9f627a6a746e80f5631955c3b3f971f23c3857bf
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2024-02-13 13:50:33 +0100

    Dapplo.Windows.User32-1.0.26, Dapplo.Windows.Input-1.0.26 (#328)
    * Added System.Reactive
    
    * Added Dapplo.Log
    
    * Added Dapplo.Windows.Common
    
    * Added Dapplo.Windows.Input
    
    * Added Dapplo.Windows.Messages
    
    * Added Dapplo.Windows.User32
    
    * Added System.Runtime.InteropServices.WindowsRuntime (ignored)
    
    * Dapplo.Log 1.4.1 -> 1.3.17
    
    * Update registry.json
    
    * Added Dapplo packages into RegistryTests (min version 1.0.26)
    
    * Update registry.json

--- src/UnityNuGet.Tests/RegistryTests.cs
+++ src/UnityNuGet.Tests/RegistryTests.cs
@@ -132,6 +132,11 @@ namespace UnityNuGet.Tests
                 @"System.Private.ServiceModel",
                 // Versions < 0.8.6 depend on LiteGuard, a deprecated dependency.
                 @"Telnet",
+                // Version < 1.0.26 depends on Microsoft.Windows.Compatibility, this one has tons of dependencies that don't target .netstandard2.0. And one of them is System.Speech that doesn't work in Unity.
+                @"Dapplo.Windows.Common",
+                @"Dapplo.Windows.Input",
+                @"Dapplo.Windows.Messages",
+                @"Dapplo.Windows.User32",
                 // It has too many versions, the minimum version is lifted so as not to process so many versions
                 @"UnitsNet.*"
             };
